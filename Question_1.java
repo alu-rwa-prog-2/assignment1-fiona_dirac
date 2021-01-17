33db9b82f8734c51abdbfa0ba9880c7965a9fc90
= <=>
2021-01-17 17:30:08 +0200

    merge files together

--- Question_1.java
+++ Question_1.java
@@ -1,8 +1,11 @@
+// Authors : Fiona Nganga and Dirac Murairi
+
 import java.util.*;
 
 public class Question_1
 {
 	public static void main(String[] args) {
+		// BMI
 	    // create a scanner object to get user input
 	    Scanner myObj = new Scanner(System.in);
 	    
@@ -21,5 +24,19 @@ public class Question_1
 		// display and calculate the BMI. BMI = Mass(kg) / height(meter) ** 2
 		double BMI = userMass / (userHeight * userHeight);
 		System.out.println(userName + " Your BMI is " + BMI);
+
+		//Perimeter and area of a rectangle
+		// We assign the length and width of the rectangle
+		int length = 38;
+		int width = 76;
+		// the formula for perimeter is double the sum of the
+		//length and the width
+		int perimeter = 2 * (length + width);
+		// the area of a rectangle is the product of its length and width
+		int area = length * width;
+
+		// We print out the perimeter and the area of the rectangle
+		System.out.println("The Perimeter is : " + perimeter);
+		System.out.println("The Area is : " + area);
 	}
 }
